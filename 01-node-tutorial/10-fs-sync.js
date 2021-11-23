b4f036782313180c9327e1079f9abe9eb46d77e8
BhargavSushant <bhargavsushant@gmail.com>
2021-11-23 17:57:40 -0500

    23

--- 01-node-tutorial/10-fs-sync.js
+++ 01-node-tutorial/10-fs-sync.js
@@ -8,18 +8,27 @@ const second = readFileSync("./01-node-tutorial/content/second.txt", "utf8");
 const testFolder = "./01-node-tutorial/";
 const fs = require("fs");
 
-fs.readdir(testFolder, (err, files) => {
-  files.forEach((file) => {
-    console.log(file);
-    writeFileSync("./01-node-tutorial/content/dir.txt", `${file}\n`, {
-      flag: "a",
+// to write contents of directory into file
+//definition: fs.readdir(path [,options],callback){}
+// path=string/buffer/url
+// options=string/object
+fs.readdir(
+  testFolder,
+  (err, files) => {
+    files.forEach((file) => {
+      // console.log(file);
+      writeFileSync("./01-node-tutorial/content/dir.txt", `${file}\n`, {
+        flag: "a",
+      });
     });
-  });
-});
+  },
+  console.log("A Callback")
+);
 
 writeFileSync(
   "./01-node-tutorial/content/result-sync.txt",
-  `Here is the result : ${first}, ${second}`,
+  `Here is the result : ${first}, ${second} ");
+  }}`,
   { flag: "a" }
 );
 console.log("done with this task");
--- 01-node-tutorial/11-fs-async.js
+++ 01-node-tutorial/11-fs-async.js
@@ -1,29 +1,34 @@
-const { readFile, writeFile } = require('fs')
+// method overridden
+const { readFile, writeFile } = require("fs");
 
-console.log('start')
-readFile('./content/first.txt', 'utf8', (err, result) => {
-  if (err) {
-    console.log(err)
-    return
-  }
-  const first = result
-  readFile('./content/second.txt', 'utf8', (err, result) => {
+console.log("start");
+readFile(
+  "./01-node-tutorial/content/first.txt" /**path/url */,
+  "utf8" /**parameters/options */,
+  /**callback function */ (err, result) => {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    const second = result
-    writeFile(
-      './content/result-async.txt',
-      `Here is the result : ${first}, ${second}`,
-      (err, result) => {
-        if (err) {
-          console.log(err)
-          return
-        }
-        console.log('done with this task')
+    const first = result;
+    readFile("./01-node-tutorial/content/second.txt", "utf8", (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
       }
-    )
-  })
-})
-console.log('starting next task')
+      const second = result;
+      writeFile(
+        "./01-node-tutorial/content/result-async.txt",
+        `Here is the result : ${first}, ${second}`,
+        (err, result) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log("done with this task");
+        }
+      );
+    });
+  }
+);
+console.log("starting next task");
